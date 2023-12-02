6aafafb948958d02dc1d25dfbdea1a312dfd91ab
RyanShihabi <ryanshihabi10@gmail.com>
2023-12-01 17:59:06 -0800

    taking sources out of ignore

--- mining.py
+++ mining.py
@@ -20,17 +20,17 @@ from mining_utils import (collect_reports,
 facility_data = generate_facility_names("./sources/facility_data/csv/all_facilities.csv")
 
 # export_data(facility_data["facility_category"], "./sources/facility_data/json/facility_category.json")
-export_data(facility_data["category_facilities"], "./sources/facility_data/json/category_facilities.json")
+# export_data(facility_data["category_facilities"], "./sources/facility_data/json/category_facilities.json")
 
-export_data(generate_paragraph_apriori(facility_data["facility_name_abbr"], "./reports-oct"), "./analysis/json/paragraph_mentions.json")
+# export_data(generate_paragraph_apriori(facility_data["facility_name_abbr"], "./reports-oct"), "./analysis/json/paragraph_mentions.json")
 
 # Get a boolean value for whether a facility was mentioned on that day
-# facility_mentions = grab_facility_mentions("./rao_reports", facility_data)
+facility_mentions = grab_facility_mentions("./rao_reports", facility_data)
 
-# df = pd.DataFrame.from_dict(facility_mentions).T
-# df.index = pd.to_datetime(df.index)
-# df = df.sort_index(ascending=True)
+df = pd.DataFrame.from_dict(facility_mentions).T
+df.index = pd.to_datetime(df.index)
+df = df.sort_index(ascending=True)
 
 # export_data(df, "./analysis/csv/facility_mentions.csv")
 
-# print(df.loc[:, "SPHERES"].sum())
+print(df.loc[:, "ARED"].sum())
