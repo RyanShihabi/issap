982fe35c55d761af1c7d648f256f35382dcbad3b
RyanShihabi <ryanshihabi10@gmail.com>
2024-02-13 12:07:22 -0800

    query bug fix

--- facility_graph.py
+++ facility_graph.py
@@ -1,9 +1,6 @@
-from facility_graph_utils import (upload_facility_itemsets, 
-                                upload_sequential_mentions,
-                                query)
+from facility_graph_utils import (upload_facility_itemsets,
+                                  upload_sequential_mentions)
 
 # upload_sequential_mentions()
 
-# upload_facility_itemsets()
-
-query("JAXA", "CSA", "agency")
+upload_facility_itemsets()
--- facility_graph_utils.py
+++ facility_graph_utils.py
@@ -4,7 +4,6 @@ import json
 import re
 import os
 from dotenv import load_dotenv
-from datetime import datetime
 from tqdm import tqdm
 
 load_dotenv(dotenv_path="secret.env")
@@ -17,53 +16,21 @@ date_uri = os.getenv("DATE_CONN")
 user = os.getenv("DATE_USER")
 date_password = os.getenv("DATE_SECRET")
 
-query = (
-    "MERGE (p1:Facility { name: $f1_name, agency: $f1_agency, category: $f1_category, module: $f1_module }) "
-    "MERGE (p2:Facility { name: $f2_name, agency: $f2_agency, category: $f2_category, module: $f2_module }) "
-    "CREATE (p1)-[k:WEIGHT]->(p2), (p2)-[j:WEIGHT]->(p1) "
-    "SET k.weight = $weight, j.weight = $weight "
-    "RETURN p1, p2"
-)
+query = """
+    MERGE (p1:Facility { name: $f1_name, agency: $f1_agency, category: $f1_category, module: $f1_module }) 
+    MERGE (p2:Facility { name: $f2_name, agency: $f2_agency, category: $f2_category, module: $f2_module }) 
+    CREATE (p1)-[k:WEIGHT]->(p2), (p2)-[j:WEIGHT]->(p1) 
+    SET k.weight = $weight 
+    RETURN p1, p2
+"""
 
-date_query = (
-    "MERGE (p1:Facility { name: $f1_name, agency: $f1_agency, category: $f1_category, module: $f1_module }) "
-    "MERGE (p2:Facility { name: $f2_name, agency: $f2_agency, category: $f2_category, module: $f2_module }) "
-    "CREATE (p1)-[k:DATE { date: $date }]->(p2) "
-    "SET k.weight = $weight "
-    "RETURN p1, p2"
-)
-
-# All attributes in a facility node
-def execute_write(tx, facility1_name, facility2_name, frequency, facility1_agency, facility2_agency, facility1_category, facility2_category, facility1_module, facility2_module):
-    result = tx.run(query, {
-        'f1_name': facility1_name,
-        'f2_name': facility2_name,
-        'weight': frequency,
-        'f1_agency': facility1_agency,
-        'f2_agency': facility2_agency,
-        'f1_category': facility1_category,
-        'f2_category': facility2_category,
-        'f1_module': facility1_module,
-        'f2_module': facility2_module,
-    })
-    return result.single()
-
-# All attributes in a date facility node
-def execute_date_write(tx, facility1_name, facility2_name, frequency, date, facility1_agency, facility2_agency, facility1_category, facility2_category, facility1_module, facility2_module):
-    result = tx.run(date_query, {
-        'f1_name': facility1_name,
-        'f2_name': facility2_name,
-        'weight': frequency,
-        'date': date,
-        'f1_agency': facility1_agency,
-        'f2_agency': facility2_agency,
-        'f1_category': facility1_category,
-        'f2_category': facility2_category,
-        'f1_module': facility1_module,
-        'f2_module': facility2_module,
-    })
-
-    return result.single()
+date_query = """
+    MERGE (p1:Facility { name: $f1_name, agency: $f1_agency, category: $f1_category, module: $f1_module }) 
+    MERGE (p2:Facility { name: $f2_name, agency: $f2_agency, category: $f2_category, module: $f2_module }) 
+    CREATE (p1)-[k:DATE { date: $date }]->(p2) 
+    SET k.weight = $weight 
+    RETURN p1, p2
+"""
 
 # Get all of the source data into the graph network
 def upload_facility_itemsets():
@@ -84,7 +51,7 @@ def upload_facility_itemsets():
 
         next(reader)
 
-        for row in tqdm(reader, total=584.0):
+        for row in tqdm(reader, total=584):
             names = re.findall(r"'([^']*)'", row[2])
             support = float(row[1])
             frequency = int(row[4])
@@ -94,19 +61,17 @@ def upload_facility_itemsets():
                 name2 = names[1].replace("-", "_").replace(" ", "_")
 
                 with GraphDatabase.driver(facility_uri, auth=(user, facility_password)) as driver:
-                    with driver.session() as session:
-                        session.execute_write(execute_write, 
-                                                        name1, 
-                                                        name2, 
-                                                        frequency,
-                                                        facility_agency[names[0]],
-                                                        facility_agency[names[1]],
-                                                        facility_category[names[0]],
-                                                        facility_category[names[1]],
-                                                        facility_module.get(names[0], "Unknown"),
-                                                        facility_module.get(names[1], "Unknown")
-                                                    )
-                    session.close()
+                    driver.execute_query(query,
+                                            f1_name = name1,
+                                            f2_name = name2,
+                                            weight = frequency,
+                                            f1_agency = facility_agency[names[0]],
+                                            f2_agency = facility_agency[names[1]],
+                                            f1_category = facility_category[names[0]],
+                                            f2_category = facility_category[names[1]],
+                                            f1_module = facility_module.get(names[0], "Unknown"),
+                                            f2_module = facility_module.get(names[1], "Unknown"),
+                                        )
                 driver.close()
 
     f.close()
@@ -145,20 +110,18 @@ def upload_sequential_mentions():
                 name2 = seq_list[i+1].replace("-", "_").replace(" ", "_")
 
                 with GraphDatabase.driver(date_uri, auth=(user, date_password)) as driver:
-                    with driver.session() as session:
-                        session.execute_write(execute_date_write, 
-                                                name1,
-                                                name2,
-                                                frequency,
-                                                date,
-                                                facility_agency[seq_list[i]],
-                                                facility_agency[seq_list[i+1]],
-                                                facility_category[seq_list[i]],
-                                                facility_category[seq_list[i+1]],
-                                                facility_module.get(seq_list[i], "Unknown"),
-                                                facility_module.get(seq_list[i+1], "Unknown")
-                                            )
-                    session.close()
+                    driver.execute_query(date_query,
+                                            f1_name = name1,
+                                            f2_name = name2,
+                                            weight = frequency,
+                                            date = date,
+                                            f1_agency = facility_agency[seq_list[i]],
+                                            f2_agency = facility_agency[seq_list[i+1]],
+                                            f1_category = facility_category[seq_list[i]],
+                                            f2_category = facility_category[seq_list[i+1]],
+                                            f1_module = facility_module.get(seq_list[i], "Unknown"),
+                                            f2_module = facility_module.get(seq_list[i+1], "Unknown"),
+                                        )
                 driver.close()
             
             completed_date_uploads.append(date)
@@ -166,26 +129,3 @@ def upload_sequential_mentions():
             with open("./sources/reports/completed_sequential_uploads.json", "w") as f:
                 json.dump(completed_date_uploads, f, indent=4)
             f.close()
-
-
-def query(f1, f2, attr):
-    agency_query = (
-        "MATCH path = (n1)-[r:DATE]->(n2) "
-        f"WHERE n1.{attr} = '{f1}' AND n2.{attr} = '{f2}' "
-        "return PROPERTIES(r);"
-    )
-
-    with GraphDatabase.driver(facility_uri, auth=(user, facility_password)) as driver:
-        records, _, _ = driver.execute_query(agency_query)
-    driver.close()
-
-    dates = []
-
-    for record in records:
-        m, d, y = record[0]['date'].split("-")
-    
-        dates.append(datetime(int(y), int(m), int(d)))
-
-    dates = sorted(dates)
-
-    print(f"{f1}->{f2}\nEarliest: {dates[0]}\nMost Recent: {dates[-1]}\nTotal: {len(dates)}")
