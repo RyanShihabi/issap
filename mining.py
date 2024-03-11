53d59410d30ad7763a2d1ebe241fc9b73df4b914
RyanShihabi <ryanshihabi10@gmail.com>
2024-03-11 15:37:02 -0700

    TVIS typo

--- mining.py
+++ mining.py
@@ -29,7 +29,7 @@ pair_dict = {name: abbr for name, abbr in facility_data["facility_name_abbr"].it
 
 apriori_list = generate_paragraph_apriori(pair_dict, "./reports-oct")
 export_data(apriori_list, "./analysis/json/paragraph_mentions_without_ARED_CEVIS_TEVIS.json")
-apriori_from_list(apriori_list, "apriori_pairs_without_ARED_CEVIS_TEVIS")
+apriori_from_list(apriori_list, "apriori_pairs_without_ARED_CEVIS_TVIS")
 
 # metrics = ["agency", "category", "module"]
 
