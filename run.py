0ec1f62ea3d33035bb101548a254cbfeac409c7a
RyanShihabi <ryanshihabi10@gmail.com>
2024-09-28 17:24:14 -0700

    CBEF and CBEF-L assigned to custom category Biology

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from mining import run_mining
 from apriori import run_apriori
 from analysis import run_analysis
+from utils.graph_utils import upload_facility_itemsets
 
 # Collect data regarding facilities and days of mentions
 print("Scraping Reports")
@@ -11,5 +12,7 @@ print("\nGenerating Apriori Pairs")
 apriori_df, apriori_without_exercise_df = run_apriori(facility_data)
 
 # Analysis on facility mentions and frequent itemsets
-print("\nGenerating Analysis")
-run_analysis(facility_data, mentions_df, apriori_df, apriori_without_exercise_df)
+# print("\nGenerating Analysis")
+# run_analysis(facility_data, mentions_df, apriori_df, apriori_without_exercise_df)
+
+upload_facility_itemsets(facility_data, apriori_df)
